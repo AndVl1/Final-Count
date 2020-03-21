92f68d23ab1c8d2b9afaec267754468eb27917b0
andvl1 <and.vladislavov@gmail.com>
2020-03-21 19:03:03 +0300

    google auth

--- app/src/main/java/com/techpark/finalcount/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/techpark/finalcount/ui/auth/AuthActivity.kt
@@ -1,23 +1,30 @@
 package com.techpark.finalcount.ui.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.techpark.finalcount.MainActivity
-import com.techpark.finalcount.databinding.ActivityAuthBinding
 import com.techpark.finalcount.R
+import com.techpark.finalcount.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var loginActivityBinding : ActivityAuthBinding
     private val mAuth = FirebaseAuth.getInstance()
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -117,6 +124,58 @@ class AuthActivity : AppCompatActivity() {
         } else !loginActivityBinding.loginInput.text.isNullOrEmpty() &&
                 !loginActivityBinding.passwordInput.text.isNullOrEmpty() &&
                 !loginActivityBinding.displayName.text.isNullOrEmpty()
+
+
+    // ----------------- GOOGLE -----------------
+    fun loginWithGoogle(view: View){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
+    }
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("GOOGLE", "Google sign in failed", e)
+
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d("GOOGLE", "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("SIGN IN", "signInWithCredential:success")
+                    toMainActivity()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("GOOGLE", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+    //-------------------------------------------
 
 
     private fun toMainActivity() {
