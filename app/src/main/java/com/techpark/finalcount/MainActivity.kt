0556427de71371395b2f5b684bba109e51025e46
Andrey <and.vladislavov@gmail.com>
2020-03-31 13:29:08 +0300

    bottom nav menu initial

--- app/src/main/java/com/techpark/finalcount/MainActivity.kt
+++ app/src/main/java/com/techpark/finalcount/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.techpark.finalcount
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.techpark.finalcount.databinding.ActivityMainBinding
+import com.techpark.finalcount.ui.dashboard.DashboardFragment
+import com.techpark.finalcount.ui.home.HomeFragment
+import com.techpark.finalcount.ui.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainBinding: ActivityMainBinding
+    private val screens: Array<Fragment> = arrayOf(
+        HomeFragment(),
+        DashboardFragment(),
+        ProfileFragment()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        mainBinding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> replaceScreen(0)
+                R.id.navigation_dashboard -> replaceScreen(1)
+                R.id.navigation_profile -> replaceScreen(2)
+            }
+            true
+        }
+        replaceScreen(0)
+    }
+
+    private fun replaceScreen(position: Int) {
+        var pos = position
+        if (pos < 0 || pos >= screens.size) {
+            pos = 0
+        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(mainBinding.container.id, screens[pos])
+            .commit()
     }
 }
---
+++ app/src/main/java/com/techpark/finalcount/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,26 @@
+package com.techpark.finalcount.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+
+class DashboardFragment : Fragment() {
+
+    private var root: View? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        if (root == null) {
+            root = TextView(activity)
+            (root as TextView).text = "Dashboard"
+        }
+        return root
+    }
+}
