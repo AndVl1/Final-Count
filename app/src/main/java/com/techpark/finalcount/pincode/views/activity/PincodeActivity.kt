f29c2f42b61a649bcfccd7a7c458c66a5b56cddf
Андрей <and.vladislavov@gmail.com>
2020-04-30 12:32:06 +0300

    pin dark

---
+++ app/src/main/java/com/techpark/finalcount/pincode/views/activity/PincodeActivity.kt
@@ -0,0 +1,12 @@
+package com.techpark.finalcount.pincode.views.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.techpark.finalcount.R
+
+class PincodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pincode)
+    }
+}
