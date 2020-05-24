484d836f9604112e2d43332337695be3780e808b
Andrey <and.vladislavov@gmail.com>
2020-05-24 22:42:11 +0300

    little color fix

--- app/src/main/java/com/techpark/finalcount/adding/AddingModule.kt
+++ app/src/main/java/com/techpark/finalcount/adding/AddingModule.kt
@@ -1,18 +1,12 @@
 package com.techpark.finalcount.adding
 
 import com.techpark.finalcount.adding.presenters.AddingPresenter
-import com.techpark.finalcount.di.ActivityScope
 import com.techpark.finalcount.di.FragmentScope
 import dagger.Binds
 import dagger.Module
 
 @Module
 abstract class AddingModule {
-
-	@ActivityScope
-	@Binds
-	internal abstract fun addingPresenter(presenter: AddingPresenter): AddingPresenter
-
 	@FragmentScope
 	@Binds
 	internal abstract fun addingFrPresenter(presenter: AddingPresenter): AddingPresenter
