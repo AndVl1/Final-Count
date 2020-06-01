d38454e16a6bcc8b99aa03771e7fa975d61d99d0
Andrey <and.vladislavov@gmail.com>
2020-06-01 11:49:36 +0300

    spent/left in plan

--- app/src/main/java/com/techpark/finalcount/plans/views/PlansHolder.kt
+++ app/src/main/java/com/techpark/finalcount/plans/views/PlansHolder.kt
@@ -1,6 +1,7 @@
 package com.techpark.finalcount.plans.views
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.view.View
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
@@ -12,23 +13,23 @@ import java.util.*
 
 class PlansHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 	var element: CardView = itemView.findViewById(R.id.planning_list_element)
-	var id: Long = 0
-	val format = SimpleDateFormat("dd.mm.YYYY", Locale.getDefault())
+	var date: Long = 0
+	private val format = SimpleDateFormat("dd.mm.YYYY", Locale.getDefault())
 
 	@SuppressLint("SetTextI18n")
 	fun bind(planning: Planning) {
-		id = planning.id
+		date = planning.id
 		element.start_date.text = format.format(planning.begin)
 		element.end_date.text = " - " + format.format(planning.end)
-		element.left_for_plan.text = (planning.planned - planning.spent).toString()
-	}
-
-	init {
-//		element.setOnClickListener { v ->
-//			Log.d("HOLDER", "click")
-//			val intent = Intent(v.context, PurchaseActivity::class.java)
-//			intent.putExtra("id", id)
-//			v.context.startActivity(intent)
-//		}
+		val left = planning.planned - planning.spent
+		element.left_for_plan.text = left.toString()
+		element.total_spent.text = planning.spent.toString()
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			if (left > 0) {
+				element.left_for_plan.setTextColor(itemView.context.getColor(R.color.green))
+			} else {
+				element.left_for_plan.setTextColor(itemView.context.getColor(R.color.red))
+			}
+		}
 	}
 }
